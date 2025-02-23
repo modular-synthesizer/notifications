3198e46fc12709a4db108a272bf7aac46464a466
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-02-23 17:57:42 +0100

    Refactor the entire application as a small script with the new exchange system

--- src/headers.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from "express";
-
-export function writeHeaders(res: Response) {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-}
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,46 @@
-import 'dotenv/config'
-import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
-import { writeHeaders } from './headers';
-import { addToRegistry, removeFromRegistry } from './registry';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import amqplib, { ConsumeMessage } from 'amqplib';
+import cors from "cors";
+import 'dotenv/config';
+import { v4 as uuid } from 'uuid'
 
 dotenv.config();
-
+function writeSSEHeaders(res: Response) {
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.setHeader('Cache-Control', 'no-cache')
+  res.setHeader('Connection', 'keep-alive')
+}
 const app: Express = express();
-const port: number = +(process.env.PORT || '4000');
+app.use(cors())
+const port = process.env.PORT || 4000;
+const { AMQP_URL } = process.env;
+(async () => {
+  const connection = await amqplib.connect(AMQP_URL || '');
+  app.get("/", cors(), async (req: Request, res: Response) => {
+    writeSSEHeaders(res)
+    const channel = await connection.createChannel();
+    function handleMessage(message: ConsumeMessage | null) {
+      if (message === null) return;
+      channel.ack(message);
+      const content: string = `data: ${message.content.toString()}\n\n`;
+      console.log(`sending to ${req.query.auth_token}`, content)
+      res.write(content);
+    }
+    const tmpUuid: string = uuid();
+    const token: string = `${req.query.auth_token}`
+    const queue: string = `${token}.${tmpUuid}`;
+    channel.assertQueue(queue, { autoDelete: true, durable: true });
+    channel.bindQueue(queue, 'production.commands', token);
+    channel.consume(queue, handleMessage);
 
-app.use(cors());
-
-app.get('/', cors(), async (req: Request, res: Response) => {
-  writeHeaders(res);
-
-  if (req.query.auth_token === undefined) {
-    res.status(403).send({ message: 'forbidden' });
-    return;
-  }
-
-  console.log("Sending the retry command");
-  res.write('retry: 0')
-
-  const token: string = `${req.query.auth_token}`;
-  const id: string = await addToRegistry(token, res);
-
-  console.log("Added a new connection for session " + token + " at index " + id)
-
-  req.on('close', () => {
-    console.log("Closing connection for token " + token + " and id " + id)
-    removeFromRegistry(token, id)
+    req.on('close', () => {
+      console.log("Closing the connection now ?");
+      channel.close();
+      res.end();
+    })
   });
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+})();
